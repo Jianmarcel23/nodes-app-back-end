d428c1a2c80ad4a70dbeaa2723bd6b3ef5aa8bd2
jian <jianmarcelhamonangan@gmail.com>
2022-03-14 16:34:18 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,45 @@
+const routes = [
+        {
+          method: 'POST',
+          path: '/notes',
+          handler: addNoteHandler,
+        },
+        {
+          method: 'GET',
+          path: '/notes',
+          handler: getAllNotesHandler,
+        },
+        {
+          method: 'GET',
+          path: '/notes/{id}',
+          handler: () => {},
+        },
+        {
+            method: 'GET',
+            path: '/notes/{id}',
+            handler: getNoteByIdHandler,
+          },
+          {
+            method: 'PUT',
+            path: '/notes/{id}',
+            handler: () => {},
+          },
+          {
+            method: 'PUT',
+            path: '/notes/{id}',
+            handler: editNoteByIdHandler,
+          },
+          {
+            method: 'DELETE',
+            path: '/notes/{id}',
+            handler: () => {},
+          },
+          {
+            method: 'DELETE',
+            path: '/notes/{id}',
+            handler: deleteNoteByIdHandler,
+         },
+      ];
+      const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require('./handler');
+   
+  module.exports = { addNoteHandler, getAllNotesHandler };
